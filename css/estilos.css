cde2f5c136a09eb8a9827133aeb2887da4d76f8e
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-25 12:03:01 -0600

    Estilos para la pagina principal del sistema

---
+++ css/estilos.css
@@ -0,0 +1,21 @@
+<?php
+$servidor = "localhost";
+$usuario = "muebles";
+$baseDatos = "Muebleria";
+$password = "MUEBLERIA";
+
+// Conectando a la base de datos
+$conexion = mysqli_connect($servidor, $usuario, $password, $baseDatos);
+
+// Verificar la conexión
+if (!$conexion) {
+    die("Error al conectar con la base de datos: " . mysqli_connect_error());
+}  
+
+// Opcional: Configuración de caracteres UTF-8
+mysqli_set_charset($conexion, "utf8");
+
+// Otras configuraciones si las necesitas
+
+?>
+
