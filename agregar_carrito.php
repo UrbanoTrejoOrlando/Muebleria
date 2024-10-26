9ff366967cfb2f46f796054e48ce23385fe299a8
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-26 10:35:15 -0600

    Codigo para agregar los productos al carrito

---
+++ agregar_carrito.php
@@ -0,0 +1,27 @@
+<?php
+include 'conexion.php';
+session_start();
+
+$codigo_barras = $_POST['codigo_barras'];
+$cantidad = $_POST['cantidad'];
+
+if (!isset($_SESSION['carrito'])) {
+    $_SESSION['carrito'] = array();
+}
+
+$producto_encontrado = false;
+foreach ($_SESSION['carrito'] as &$producto) {
+    if ($producto['codigo_barras'] == $codigo_barras) {
+        $producto['cantidad'] += $cantidad;
+        $producto_encontrado = true;
+        break;
+    }
+}
+
+if (!$producto_encontrado) {
+    $_SESSION['carrito'][] = array('codigo_barras' => $codigo_barras, 'cantidad' => $cantidad);
+}
+
+header('Location: index.php');
+?>
+
