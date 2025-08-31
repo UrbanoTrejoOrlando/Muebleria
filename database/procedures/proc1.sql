16075393e9eac4bfae3445066da30a5312e66d8b
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-08-30 20:37:25 -0600

    Carpeta database

---
+++ database/procedures/proc1.sql
@@ -0,0 +1,26 @@
+
+DROP PROCEDURE IF EXISTS mostrarproductos;
+
+DELIMITER //
+
+CREATE PROCEDURE mostrarproductos()
+BEGIN
+    SELECT
+        productos.codigo_barras, 
+        productos.Nombre, 
+        productos.descripcion, 
+        productos.precio, 
+        productos.stock_minimo, 
+        productos.stock_maximo,
+        marcas.nombre,
+        categorias.nombre,
+        colores.nombre
+    FROM productos
+    INNER JOIN marcas ON productos.marca_id = marcas.id
+    INNER JOIN categorias ON productos.categoria_id = categorias.id
+    INNER JOIN colores ON productos.color_id = colores.id;
+END //
+DELIMITER ;;
+
+
+CALL mostrarproductos;
