8d56570b58759db57ec3d35fa2045eef167d583e
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-09-23 22:15:46 -0600

    Inserciones a las tablas

--- database/insercion.sql
+++ database/insercion.sql
@@ -1,4 +1,3 @@
-INSERT INTO materiales (nombre) VALUES ('Vidrio');
 INSERT INTO materiales (nombre) VALUES ('Acero');
 INSERT INTO materiales (nombre) VALUES ('Plástico');
 INSERT INTO materiales (nombre) VALUES ('Fibra natural');
