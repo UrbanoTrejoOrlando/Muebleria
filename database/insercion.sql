2765a67596f1cda2ea117d6f9f426fffc2469318
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-09-24 22:05:36 -0600

    Inserciones a la base de datos

--- database/insercion.sql
+++ database/insercion.sql
@@ -1,4 +1,3 @@
-VALUES ('3456789012345', 'Sofá de cuero', 'Sofá de tres plazas en cuero genuino, confortable y elegante.', 500.00, 30, 3, 50, 3, 3, 8);
 
 -- Producto 4
 INSERT INTO productos (codigo_barras, nombre, descripcion, precio, stock, stock_minimo, stock_maximo, marca_id, categoria_id, color_id)
