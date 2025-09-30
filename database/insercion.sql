54a6ad57993649831f44158b5fbcbd8bd20b6f44
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-09-29 21:12:13 -0600

    Inserciones a la base de datos

--- database/insercion.sql
+++ database/insercion.sql
@@ -1,4 +1,3 @@
-('Carlos Sánchez', 'Plaza 789, Ciudad', '555-9101', 'orlandourbanotrejo@gmail.com'),
 ('Ana López', 'Boulevard 101, Ciudad', '555-1121', 'ana.lopez@example.com'),
 ('Luis Fernández', 'Camino 202, Ciudad', '555-3141', 'luis.fernandez@example.com');
 
