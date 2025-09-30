da2d55ff39816eaa9f81962c60267eade5361f68
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-09-29 21:11:12 -0600

    Inserciones a la base de datos

--- database/insercion.sql
+++ database/insercion.sql
@@ -1,4 +1,3 @@
--- Producto 10
 INSERT INTO productos (codigo_barras, nombre, descripcion, precio, stock, stock_minimo, stock_maximo, marca_id, categoria_id, color_id)
 VALUES ('0123456789012', 'Lámpara de pie', 'Lámpara de pie moderna en acero y tela, iluminación ambiental.', 80.00, 50, 5, 100, 10, 10, 1);
 
